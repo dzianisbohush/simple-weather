977112d26ba4f5ed06b1107f3489fc2cf6a9834d
dmbogush91@gmail.com <Git2016!>
2019-05-29 11:32:42 +0300

    Add styled-components

--- src/weather/component.js
+++ src/weather/component.js
@@ -1,9 +1,52 @@
 import React, { PureComponent } from 'react';
+import styled from 'styled-components';
 import { getCurrentPosition, getWeather } from '../data/api';
 import {
   WEATHER_SOURCE_APIXU_NAME,
   WEATHER_SOURCE_STORMGLASS_NAME
 } from '../data/constants';
+
+const MainWrapper = styled.div`
+  position: relative;
+  height: 100vh;
+`;
+
+const WeatherWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`;
+
+const LocationInput = styled.input`
+  margin: 1em 0 0.7em 0;
+  display: block;
+  border: none;
+  border-bottom: 1px #a3a3a3 solid;
+`;
+
+const SubmitInput = styled.input`
+  border-radius: 5px;
+`;
+
+const AirTemperatureTitle = styled.p`
+  font-size: 1em;
+  margin-bottom: 0;
+`;
+
+const AirTemperatureValue = styled.p`
+  font-size: 1.5em;
+  margin: 0;
+`;
+
+const WeatherSourceFieldset = styled.fieldset`
+  border: none;
+`;
 
 class WeatherComponent extends PureComponent {
   state = {
@@ -64,38 +107,55 @@ class WeatherComponent extends PureComponent {
     } = this.state;
 
     return (
-      <div className="weather-wrapper">
-        <h1>{selectedLocation.toUpperCase()}</h1>
-        <form onSubmit={this.handleFormSubmit}>
-          <p>Weather source: </p>
-          <label htmlFor={WEATHER_SOURCE_APIXU_NAME}>Apixu API</label>
-          <input
-            type="radio"
-            id={WEATHER_SOURCE_APIXU_NAME}
-            value={WEATHER_SOURCE_APIXU_NAME}
-            onChange={this.handleChangeWeatherSource}
-            checked={weatherSource === WEATHER_SOURCE_APIXU_NAME}
-          />
-          <label htmlFor={WEATHER_SOURCE_STORMGLASS_NAME}>Stormglass API</label>
-          <input
-            type="radio"
-            id={WEATHER_SOURCE_STORMGLASS_NAME}
-            value={WEATHER_SOURCE_STORMGLASS_NAME}
-            onChange={this.handleChangeWeatherSource}
-            checked={weatherSource === WEATHER_SOURCE_STORMGLASS_NAME}
-          />
-          <input
-            id="location"
-            name="location"
-            placeholder="Input city name"
-            value={cityNameInputValue}
-            onChange={this.handleInputChange}
-            required
-          />
-          <input type="submit" value="Get Weather" />
-        </form>
-        {currentAirTemperature.length > 0 && <p>{currentAirTemperature} °C</p>}
-      </div>
+      <MainWrapper>
+        <WeatherWrapper>
+          <h1>{selectedLocation.toUpperCase()}</h1>
+          <form onSubmit={this.handleFormSubmit}>
+            <label htmlFor="weather_source">Weather source:</label>
+            <WeatherSourceFieldset id="weather_source">
+              <div>
+                <input
+                  type="radio"
+                  id={WEATHER_SOURCE_APIXU_NAME}
+                  value={WEATHER_SOURCE_APIXU_NAME}
+                  onChange={this.handleChangeWeatherSource}
+                  checked={weatherSource === WEATHER_SOURCE_APIXU_NAME}
+                />
+                <label htmlFor={WEATHER_SOURCE_APIXU_NAME}>Apixu API</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  id={WEATHER_SOURCE_STORMGLASS_NAME}
+                  value={WEATHER_SOURCE_STORMGLASS_NAME}
+                  onChange={this.handleChangeWeatherSource}
+                  checked={weatherSource === WEATHER_SOURCE_STORMGLASS_NAME}
+                />
+                <label htmlFor={WEATHER_SOURCE_STORMGLASS_NAME}>
+                  Stormglass API
+                </label>
+              </div>
+            </WeatherSourceFieldset>
+            <LocationInput
+              id="location"
+              name="location"
+              placeholder="Input city name"
+              value={cityNameInputValue}
+              onChange={this.handleInputChange}
+              required
+            />
+            <SubmitInput type="submit" value="Get Weather" />
+          </form>
+          {currentAirTemperature.length > 0 && (
+            <React.Fragment>
+              <AirTemperatureTitle>Air temperature:</AirTemperatureTitle>
+              <AirTemperatureValue>
+                {currentAirTemperature} °C
+              </AirTemperatureValue>
+            </React.Fragment>
+          )}
+        </WeatherWrapper>
+      </MainWrapper>
     );
   }
 }
