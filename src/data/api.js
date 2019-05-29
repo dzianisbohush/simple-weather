0d1e735490fbaebe8b3b354f483dd41e36338724
dmbogush91@gmail.com <Git2016!>
2019-05-29 15:05:28 +0300

    Added aliases

--- src/data/api.js
+++ src/data/api.js
@@ -7,7 +7,7 @@ import {
   WEATHER_STORMGLASS_API_KEY,
   OPENCAGEDATA_BASE_URL,
   OPENCAGEDATA_API_KEY
-} from './constants';
+} from 'data/constants';
 
 export const getCoordinatesFromLocationName = async locationName => {
   try {
--- src/weather/component.js
+++ src/weather/component.js
@@ -1,10 +1,10 @@
 import React, { PureComponent } from 'react';
 import styled from 'styled-components';
-import { getCurrentPosition, getWeather } from '../data/api';
+import { getCurrentPosition, getWeather } from 'data/api';
 import {
   WEATHER_SOURCE_APIXU_NAME,
   WEATHER_SOURCE_STORMGLASS_NAME
-} from '../data/constants';
+} from 'data/constants';
 
 const MainWrapper = styled.div`
   position: relative;
